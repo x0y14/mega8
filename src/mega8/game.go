0367434585171a57d8bbe03bdcfa061c6fd829ed
x0y14 <ya01ma08@gmail.com>
2021-11-25 21:38:59 +0900

    metallを読み込んでみる

--- src/mega8/game.go
+++ src/mega8/game.go
@@ -2,6 +2,9 @@ package mega8
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/x0y14/mega8/src/mega8/character"
+	"github.com/x0y14/mega8/src/mega8/character/metall"
+	"github.com/x0y14/mega8/src/mega8/projector"
 )
 
 type Game struct {
@@ -9,23 +12,35 @@ type Game struct {
 	screenHeight int
 	keys         []ebiten.Key
 	count        int
+	entities     []character.Character
 }
 
 func NewGame(screenWidth, screenHeight int) *Game {
+
+	metall1 := metall.New()
+
 	return &Game{
 		screenWidth:  screenWidth,
 		screenHeight: screenHeight,
 		keys:         []ebiten.Key{},
 		count:        0,
+		entities:     []character.Character{*metall1},
 	}
 }
 
 func (g *Game) Update() error {
 	g.count++
+	for _, entity := range g.entities {
+		entity.CountUp()
+	}
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {}
+func (g *Game) Draw(screen *ebiten.Image) {
+	for _, entity := range g.entities {
+		projector.DrawCharacter(screen, entity)
+	}
+}
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return g.screenWidth, g.screenHeight
