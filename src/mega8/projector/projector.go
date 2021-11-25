abcbc7fa1b36f720d417cbe42cd1847bb7a93198
x0y14 <ya01ma08@gmail.com>
2021-11-25 21:39:17 +0900

    描写用関数追加

---
+++ src/mega8/projector/projector.go
@@ -0,0 +1,22 @@
+package projector
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/x0y14/mega8/src/mega8/character"
+	"image"
+)
+
+func DrawCharacter(screen *ebiten.Image, entity character.Character) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(float64(entity.OffsetX), float64(entity.OffsetY))
+
+	nowMot := entity.NowMotion()
+	nowFrame := nowMot.Frames[nowMot.FramePos]
+	nowSheet := nowFrame.Sheet
+	img := nowSheet.SubImage(image.Rect(
+		nowFrame.OriginX,
+		nowFrame.OriginY,
+		nowFrame.OriginX+nowFrame.Width,
+		nowFrame.OriginY+nowFrame.Height)).(*ebiten.Image)
+	screen.DrawImage(img, op)
+}
