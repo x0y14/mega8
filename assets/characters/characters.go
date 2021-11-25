4f3a653b043a8d8c46aa2585241a156cb134a538
x0y14 <ya01ma08@gmail.com>
2021-11-25 21:38:38 +0900

    assetの読み込み方法を変更

--- assets/characters/characters.go
+++ assets/characters/characters.go
@@ -1,12 +1,36 @@
 package characters
 
 import (
+	"bytes"
 	_ "embed"
+	"github.com/hajimehoshi/ebiten/v2"
+	"image"
 	_ "image/png"
+	"log"
 )
 
-//go:embed mega-man.png
+//go:embed megaman.png
 var MegaManSpritesSheetBytes []byte
+var MegaManSpritesSheet *ebiten.Image
 
 //go:embed metall.png
 var MetallSpritesSheetBytes []byte
+var MetallSpritesSheet *ebiten.Image
+
+func init() {
+	// megaman
+	buf := bytes.NewReader(MegaManSpritesSheetBytes)
+	img, _, err := image.Decode(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	MegaManSpritesSheet = ebiten.NewImageFromImage(img)
+
+	// metall
+	buf = bytes.NewReader(MetallSpritesSheetBytes)
+	img, _, err = image.Decode(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	MetallSpritesSheet = ebiten.NewImageFromImage(img)
+}
