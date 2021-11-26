4e845709e62157f874a26cca807849b0148c04ec
x0y14 <ya01ma08@gmail.com>
2021-11-26 13:25:35 +0900

    core

--- cmd/mega8/main.go
+++ cmd/mega8/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	_ "embed"
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/x0y14/mega8/src/mega8"
+	"github.com/x0y14/mega8/src/mega8/core"
 	_ "image/png"
 	"log"
 )
@@ -16,7 +16,7 @@ const (
 func main() {
 	ebiten.SetWindowSize(screenWidth*1.5, screenHeight*1.5)
 	ebiten.SetWindowTitle("mega8")
-	gameMega8 := mega8.NewGame(screenWidth, screenHeight)
+	gameMega8 := core.NewGame(screenWidth, screenHeight)
 	if err := ebiten.RunGame(gameMega8); err != nil {
 		log.Fatal(err)
 	}
